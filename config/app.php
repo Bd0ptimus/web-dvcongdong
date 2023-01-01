d26ee8947879f1bbb2e9f082065aec6f3cd90723
Bùi Thế Dũng <thedung.work.1292@gmail.com>
2023-01-01 19:23:56 +0300

    zalo auth config

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        \SocialiteProviders\Manager\ServiceProvider::class,
+
 
 
         /*
