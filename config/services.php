fd648caed94c3bcc0345c3c8ddbefc1b7e736a9e
Bùi Thế Dũng <thedung.work.1292@gmail.com>
2023-01-01 19:02:33 +0300

    add service config

--- config/services.php
+++ config/services.php
@@ -42,10 +42,16 @@ return [
         'redirect' => env('FACEBOOK_REDIRECT'),
     ],
 
-    'vk' => [
-        'client_id' => env('VK_CLIENT_ID'),
-        'client_secret' => env('VK_CLIENT_SECRET'),
-        'redirect' => env('VK_REDIRECT'),
+    // 'vk' => [
+    //     'client_id' => env('VK_CLIENT_ID'),
+    //     'client_secret' => env('VK_CLIENT_SECRET'),
+    //     'redirect' => env('VK_REDIRECT'),
+    // ],
+
+    'zalo' => [
+        'client_id' => env('ZALO_CLIENT_ID'),
+        'client_secret' => env('ZALO_CLIENT_SECRET'),
+        'redirect' => env('ZALO_REDIRECT'),
     ],
 
 ];
